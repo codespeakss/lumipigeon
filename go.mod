8938fbc09ffd217e5707da9cc2a35eee26fe8ad0
codespeakss <dd@ci.com>
2024-11-03 18:51:12 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module lumipigeon
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "log"
+    "time"
+)
+
+func main() {
+    for {
+        log.Println("fmt")
+        time.Sleep(time.Second)
+    }
+}
